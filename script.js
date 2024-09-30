0c8b138ad99db2e6823d2c84ba8ca44ed390a767
Padurariu Riccardo <padurariuriccardo.github@gmail.com>
2024-09-30 22:42:54 +0300

    Added new feature,hoping to solve some bugs soon

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const arrayList = [];
 
-
 function update(){
   let HTMLList = '';
   arrayList.forEach((attribute,index) => {
@@ -10,16 +9,27 @@ function update(){
             <p class="task-attribute num-corner">${index+1}</p>
           </div>
           <div class="task-name-container">
-            <p class="task-attribute">${attribute.name}</p>
+            <p class="task-attribute name-js">${attribute.name}</p>
           </div>
           <div class="due-date">
-            <p class="task-attribute">${attribute.due_date}</p>
+            <p class="task-attribute date-js">${attribute.due_date}</p>
           </div>
           <div class="status-container">
             <p class="task-attribute status-corner">${attribute.status}</p>
           </div>
           <div class="delete-buttons-container">
             <button class="delete-button" onclick="arrayList.splice(${index},1);update();">Delete</button>
+          </div>
+          <div class="edit-buttons-container">
+            <button class="edit-button" onclick="
+              document.querySelector('.edit-task')
+                .classList.add('home');
+              const name = document.querySelector('.task-name-js');
+              const date = document.querySelector('.task-date-js');
+              name.value = document.querySelector('.name-js').innerHTML;
+              date.value = document.querySelector('.date-js').innerHTML;
+              curr_index = ${index};
+            ">Edit</button>
           </div>
       </div>
     `;
@@ -33,6 +43,10 @@ function update(){
     .addEventListener('click',() => {
       document.querySelector('.add-task')
         .classList.add('home');
+      const name = document.querySelector('.task-name-js');
+      const date = document.querySelector('.task-date-js');
+      name.value = '';
+      date.value = '';
     });
 
   document.querySelector('.exit-button')
@@ -47,6 +61,18 @@ function update(){
       document.querySelector('.task-date-js').value = '';
     });
 
+    document.querySelector('.exit-edit-button')
+    .addEventListener('click',() => {
+      document.querySelector('.edit-task')
+        .classList.remove('home');
+      document.querySelector('.name-error-js')
+        .innerHTML = '';
+      document.querySelector('.date-error-js')
+        .innerHTML = '';
+      //document.querySelector('.task-name-js').value = '';
+      //document.querySelector('.task-date-js').value = '';
+    });
+
   document.querySelector('.add-task-modal-button')
     .addEventListener('click', () => {
       // Storing in variables the task name and task due date
@@ -54,8 +80,62 @@ function update(){
       const date = document.querySelector('.task-date-js');
       // Checking if the inputs aren't null
       if(name.value !== '' && date.value !== ''){
+        // The status is based on the current date and the due date
+        const curr_date = new Date(); // getting the current date
+        const due = new Date(date.value); // transforming the due date from a string into a class object
+        let sta = '';
+        if((due.getFullYear() === curr_date.getFullYear()) && (due.getMonth() === curr_date.getMonth()) && (due.getDay()-curr_date.getDay()===1)){
+          sta = 'DUE SOON';
+        }else if((due.getFullYear() === curr_date.getFullYear()) && (due.getMonth() === curr_date.getMonth()) && (due.getDay()-curr_date.getDay()<=0)){
+          document.querySelector('.date-error-js')
+            .innerHTML = '*Enter a valid task date';
+          sta = 'ERROR';
+        }else{
+          sta = 'OK';
+        }
+        if(sta === 'OK' || sta === 'DUE SOON'){
+          const task = {
+            name: name.value,
+            due_date: date.value,
+              status: sta
+          };
+          arrayList.push(task);
+          update();
+          name.value = '';
+          date.value = '';
+          document.querySelector('.name-error-js')
+            .innerHTML = '';
+          document.querySelector('.date-error-js')
+            .innerHTML = '';
+          // Removing the opacity from the add task section
+          document.querySelector('.add-task')
+            .classList.remove('home');
+          }
+      } else if(name.value === '' && date.value === ''){
+        document.querySelector('.name-error-js')
+          .innerHTML = '*Enter a task name';
+        document.querySelector('.date-error-js')
+          .innerHTML = '*Enter a task date';
+      } else if(date.value === ''){
+        document.querySelector('.date-error-js')
+          .innerHTML = '*Enter a task date';
+      } else if(name.value === ''){
+        document.querySelector('.name-error-js')
+          .innerHTML = '*Enter a task name';
+      }
+    });
+
+
+
+    document.querySelector('.edit-js')
+      .addEventListener('click',() => {
+        // Storing in variables the task name and task due date
+      const name = document.querySelector('.task-edit-name-js');
+      const date = document.querySelector('.task-edit-date-js');
+      // Checking if the inputs aren't null
+      if(name.value !== '' && date.value !== ''){
         // Removing the opacity from the add task section
-        document.querySelector('.add-task')
+        document.querySelector('.edit-task')
           .classList.remove('home');
         // The status is based on the current date and the due date
         const curr_date = new Date(); // getting the current date
@@ -63,8 +143,9 @@ function update(){
         let sta = '';
         if((due.getFullYear() === curr_date.getFullYear()) && (due.getMonth() === curr_date.getMonth()) && (due.getDay()-curr_date.getDay()===1))
           sta = 'DUE SOON';
-        else if((due.getFullYear() === curr_date.getFullYear()) && (due.getMonth() === curr_date.getMonth()) && (due.getDay()-curr_date.getDay()<0))
-          sta = 'DUE';
+        else if((due.getFullYear() === curr_date.getFullYear()) && (due.getMonth() === curr_date.getMonth()) && (due.getDay()-curr_date.getDay()<=0))
+          document.querySelector('.date-error-js')
+            .innerHTML = '*Enter a valid task date';
         else
           sta = 'OK';
         const task = {
@@ -72,7 +153,8 @@ function update(){
           due_date: date.value,
           status: sta
         };
-        arrayList.push(task);
+        arrayList[curr_index] = task;
+        console.log(arrayList[curr_index]);
         update();
         name.value = '';
         date.value = '';
@@ -92,4 +174,4 @@ function update(){
         document.querySelector('.name-error-js')
           .innerHTML = '*Enter a task name';
       }
-    });
+      }); 
